7b29bf8a151fc3b43faac0455343acd03612f7de
unknown <utpalforever@gmail.com>
2019-12-01 00:32:24 +0530

    first commit

---
+++ src/main/java/SikuliConceptTesting/SikuliConceptTesting/App.java
@@ -0,0 +1,13 @@
+package SikuliConceptTesting.SikuliConceptTesting;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
